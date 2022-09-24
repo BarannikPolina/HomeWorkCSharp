c864f1fb43aed9dec22e7a547559bbad059f4bf3
Barannik Polina <barannik.polina@mail.ru>
2022-09-24 10:44:51 +0300

    Добавили третью задачу

---
+++ HomeWork02/Example03/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход семь чисел
+// и находит их среднее арифметическое 
+
+Console.WriteLine ("Введите семь чисел");
+int [] array = new int [7];
+for (int i = 0; i < 7; i++) 
+{
+array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int sum = 0;
+for (int i = 0; i < 7; i++) 
+{
+sum = sum + array[i];
+}
+int average = sum/7;
+Console.WriteLine("Среднее арифметическое = " + average);
+    
+
+
