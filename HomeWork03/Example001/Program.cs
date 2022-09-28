5c8204a0e76662d2b674d632f6f80cd37fde5db0
Barannik Polina <barannik.polina@mail.ru>
2022-09-28 13:57:31 +0300

    Добавили задачу: нахождение таблицы кубов чисел

---
+++ HomeWork03/Example001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите метод, который принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int value = 3;
+for (int i = 1; i <= value; i++) 
+{
+Console.WriteLine(i * i * i);
+}
+
+//Console.WriteLine(Method);
+
+//void Method(int value);
+//int value = 5;
+//for (int i = 1; i <= value; i++)
+//{
+//Console.WriteLine(i * i * i);
+//}
