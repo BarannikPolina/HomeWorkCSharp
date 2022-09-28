321b25627f04a79b4097ddb2506f2665df19df1a
Barannik Polina <barannik.polina@mail.ru>
2022-09-28 14:41:08 +0300

    Добавили задачу: Является ли число палиндромом

---
+++ HomeWork03/Example002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Является ли число палиндромом 
+
+int num = 45954;
+int[] array = new int[5];
+for (int i = 4; i >= 0; i--) 
+{
+    array[i] = num % 10;
+    num = num/10;
+}
+bool isPolindrom = true;
+for(int i = 0; i < 5; i++) 
+{
+    if(array[i] != array[5-i-1])
+{
+    isPolindrom=false;
+    break;
+}
+}
+    Console.Write( isPolindrom ? "Это полиндром ":" Это не полиндром");
