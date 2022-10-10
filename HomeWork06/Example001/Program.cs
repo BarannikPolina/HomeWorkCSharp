e4418e5c8bb575b8b492b5f085a32277c7381c80
Barannik Polina <barannik.polina@mail.ru>
2022-10-10 19:21:11 +0300

    Добавили задачу №41

---
+++ HomeWork06/Example001/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача № 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+
+Console.WriteLine("Введите числа");
+String str = Console.ReadLine();
+String [] arrFromString = str.Split(" ");
+for (int i = 0 ;  i < arrFromString.Length; i++) 
+{
+
+}
+
+int count = 0;
+int [] arr = new int [arrFromString.Length];
+for (int i = 0; i < arrFromString.Length; i++) 
+{
+    arr [i] = Int32.Parse(arrFromString[i]);
+    if (arr[i] > 0) 
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
+    
