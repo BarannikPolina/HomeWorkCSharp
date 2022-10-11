5e9fe95d82cfe7074b7499780ea74ee0678e9a9e
Barannik Polina <barannik.polina@mail.ru>
2022-10-11 14:09:06 +0300

    Добавили задачу №47

---
+++ HomeWork07/Example001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+
+Console.WriteLine("Введите 12 случайных вещественных чисел");
+int [,] array = new int[3, 4];
+for (int i = 0; i < 3; i++) 
+{
+    Console.WriteLine();
+    for (int j = 0; j < 4; j++) 
+    {
+    array[i, j] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(array[i, j] + " ");
+    }
+}
